4370c2fc11e837849bd43377a3ea6a3a83f08944
Abi <36452265+amorimferreira@users.noreply.github.com>
2019-10-16 23:01:46 +0100

    add foreign key user_id to habits

--- db/migrate/20191014212239_create_habits.rb
+++ db/migrate/20191014212239_create_habits.rb
@@ -1,6 +1,7 @@
 class CreateHabits < ActiveRecord::Migration[6.0]
   def change
     create_table :habits do |t|
+      t.integer :user_id
       t.string :title
       t.string :description
       t.integer :difficulty
