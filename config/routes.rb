540b5ca27df6866abba55690729990e9ea877597
Abi <36452265+amorimferreira@users.noreply.github.com>
2019-10-24 16:32:29 -0400

    root page set to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  
   devise_for :users, controllers: { registrations: 'users/registrations'}
   resources :users do
     resources :habits
   end
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
